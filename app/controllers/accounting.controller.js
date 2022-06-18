667a9f9f4bf4d9a01e0e05db50bf46de0b1f317e
Austin Wong <38020337+mimo1112@users.noreply.github.com>
2022-06-18 01:24:10 -0700

    updating controllers

---
+++ app/controllers/accounting.controller.js
@@ -0,0 +1,15 @@
+const accounting = require("../models/accounting.model.js");
+// Create and Save a new accounting
+exports.create = (req, res) => {};
+// Retrieve all accounting from the database (with condition).
+exports.findAll = (req, res) => {};
+// Find a single accounting with a id
+exports.findOne = (req, res) => {};
+// find all published accounting
+exports.findAllPublished = (req, res) => {};
+// Update a accounting identified by the id in the request
+exports.update = (req, res) => {};
+// Delete a accounting with the specified id in the request
+exports.delete = (req, res) => {};
+// Delete all accounting from the database.
+exports.deleteAll = (req, res) => {};
