d0f9f5e071cd6d0170544bd34d206e332f43c70e
Austin Wong <38020337+mimo1112@users.noreply.github.com>
2022-06-19 02:27:01 -0700

    adding in models for tables

--- app/models/business.model.js
+++ app/models/business.model.js
@@ -1,9 +1,22 @@
 const sql = require("./db.js");
 // constructor
 const business = function (business) {
-  this.fname_of_business = business.fname_of_business;
+  this.businessID = business.businessID;
+  this.business_name = business.business_name;
+  this.owner_fname = business.owner_fname;
+  this.owner_lname = business.owner_lname;
+  this.business_profile = business.business_profile;
+  this.email = business.email;
+  this.phone_number = business.phone_number;
+  this.rate_per_hour = business.rate_per_hour;
+  this.location = business.location;
+  this.keywords = business.keywords;
+  this.education = business.education;
+  this.pictures = business.pictures;
   this.description = business.description;
-  this.published = business.published;
+  this.generalID = business.generalID;
+  
+  this.generalID = business.generalID;
 };
 business.create = (newbusiness, result) => {
   sql.query("INSERT INTO business SET ?", newbusiness, (err, res) => {
@@ -47,6 +60,7 @@ business.getAll = (title, result) => {
     result(null, res);
   });
 };
+/*
 business.getAllPublished = (result) => {
   sql.query("SELECT * FROM business WHERE published=true", (err, res) => {
     if (err) {
@@ -58,10 +72,11 @@ business.getAllPublished = (result) => {
     result(null, res);
   });
 };
+*/
 business.updateById = (id, business, result) => {
   sql.query(
-    "UPDATE business SET title = ?, description = ?, published = ? WHERE id = ?",
-    [business.title, business.description, business.published, id],
+    "UPDATE business SET business_name = ?, owner_fname = ?, owner_lname = ?, business_profile = ?, email = ?, phone_number = ?, rate_per_hour = ?, location = ?, keywords = ?, education = ?, pictures = ?, description = ? WHERE id = ?",
+    [business.fname_of_business, business.lname_of_business, business.email, business.phone_number, business.location, business.business_profile, id],
     (err, res) => {
       if (err) {
         console.log("error: ", err);
