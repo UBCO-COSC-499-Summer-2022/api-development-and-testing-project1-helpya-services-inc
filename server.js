832de921951a3153265219558438fec4f6aaa0a1
wasmiester <wasmiester@gmail.com>
2022-07-05 23:15:51 -0700

    module exports removed from app listener

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ require("./app/routes/payment.routes.js")(app);
 require("./app/routes/recentSearches.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-module.exports=app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
