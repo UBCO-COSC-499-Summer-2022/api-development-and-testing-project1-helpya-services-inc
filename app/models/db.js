91303805568a35032e68ab398e64f53ed1a961b3
wasmiester <wasmiester@gmail.com>
2022-07-07 01:37:17 -0700

    fixed onccection

--- app/models/db.js
+++ app/models/db.js
@@ -11,8 +11,19 @@ const connection = mysql.createPool({
   debug: false,
 });
 // open the MySQL connection
-connection.getConnection(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+connection.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
 });
 module.exports = connection;
