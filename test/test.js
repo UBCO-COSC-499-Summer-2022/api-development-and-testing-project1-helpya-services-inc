aef04f2b1e0625090f0876865834d41b6ac2a2a6
wasmiester <wasmiester@gmail.com>
2022-07-14 16:38:52 -0700

    ijh

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require("assert");
 let chai = require("chai");
 const { ChildProcess } = require("child_process");
 let chaiHttp = require("chai-http");
-let server = require("../server.js");
+let server = require("../server");
 chai.should();
 chai.use(chaiHttp);
 
