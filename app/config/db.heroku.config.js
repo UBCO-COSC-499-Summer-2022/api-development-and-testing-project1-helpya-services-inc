aaa301eaa5596862f8892c8bf45a6c311e40ea5f
wasmiester <wasmiester@gmail.com>
2022-07-22 15:46:45 -0700

    updated db name

--- app/config/db.heroku.config.js
+++ app/config/db.heroku.config.js
@@ -2,6 +2,6 @@ module.exports = {
   HOST: "us-cdbr-east-06.cleardb.net",
   USER: "be51542c7ae965",
   PASSWORD: "8fbdc8b3",
-  DB: "helpyadb",
+  DB: "heroku_7d45a1c09c1d741",
   port: "3306",
 };
