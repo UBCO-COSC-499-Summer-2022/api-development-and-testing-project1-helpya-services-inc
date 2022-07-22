ecfc6eadc2582101aa1232d50839581c28e67a87
wasmiester <wasmiester@gmail.com>
2022-07-22 14:35:36 -0700

    setting db port

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 // Create a connection to the database
+const PORT = process.env.PORT || 3306;
 const connection = mysql.createPool({
   connectionLimit: 100,
   host: dbConfig.HOST,
