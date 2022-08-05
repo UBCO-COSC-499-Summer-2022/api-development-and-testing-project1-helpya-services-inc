756ac62b92fc1d30ac89969bf24a160884e16c99
Austin <austinwong1@yahoo.ca>
2022-08-04 21:04:28 -0700

    adding in pagination

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 var corsOptions = {
   origin: "http://localhost:3306",
 };
+const mysql = require("mysql");
+const sql = require("./posts");
+db.once("open", async () => {
+  if ((await Post.countDocuments().exec()) > 0) return;
+
+  Promise.all("api/controllers/ads.controller.js/create");
+});
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -19,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 // }).unless({
 //   path: auth.JWT_CONFIG.path
 // }))
-// validation token 
+// validation token
 // app.use((req,res,next)=>{
 //   validationTokenAuth(req,res,next)
 // })
@@ -39,8 +47,46 @@ require("./app/routes/recentSearches.routes.js")(app);
 require("./app/routes/education_history.routes.js")(app);
 require("./app/routes/job_type.routes.js")(app);
 require("./app/routes/login.routes.js")(app);
+
+app.get("/posts", paginatedResults(Post), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
+}
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-module.exports=app.listen(PORT, () => {
+module.exports = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
